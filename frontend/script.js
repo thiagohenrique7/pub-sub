ed4e849662a8319f049bcf082af5b6a59156a3b1
Thiago Nogueira <thiagohenriquean@gmail.com>
2023-08-20 23:21:49 -0300

    add latency

--- frontend/script.js
+++ frontend/script.js
@@ -1,30 +1,57 @@
-let socket;
-let currentChannel;
+let soquete;
+let canalAtual;
+let tempoInicioTesteLatencia;
 
-function connectToChannel() {
-    const channelInput = document.getElementById("channelInput");
-    currentChannel = channelInput.value;
-    socket = new WebSocket(`ws://localhost:8080/ws?canal=${currentChannel}`);
+function conectarAoCanal() {
+    const entradaCanal = document.getElementById("channelInput");
+    canalAtual = entradaCanal.value;
+    soquete = new WebSocket(`ws://localhost:8080/ws?canal=${canalAtual}`);
 
-    socket.onopen = function(event) {
-        console.log(`Conectado ao canal: ${currentChannel}`);
+    soquete.onopen = function(evento) {
+        console.log(`Conectado ao canal: ${canalAtual}`);
         document.getElementById("chatArea").style.display = "block";
+        document.getElementById("latencyTestArea").style.display = "block";
     };
 
-    socket.onmessage = function(event) {
-        const message = event.data;
-        const messageBox = document.getElementById("messageBox");
-        messageBox.innerHTML += `<p>${message}</p>`;
+    soquete.onmessage = function(evento) {
+        const mensagem = evento.data;
+        const caixaMensagens = document.getElementById("messageBox");
+        if (mensagem === "TesteDeLatencia") {
+            tratarRespostaTesteLatencia();
+        } else {
+            caixaMensagens.innerHTML += `<p>${mensagem}</p>`;
+        }
     };
 
-    socket.onclose = function(event) {
-        console.log(`Desconectado do canal: ${currentChannel}`);
+    soquete.onclose = function(evento) {
+        console.log(`Desconectado do canal: ${canalAtual}`);
     };
 }
 
-function sendMessage() {
-    const messageInput = document.getElementById("messageInput");
-    const message = messageInput.value;
-    socket.send(message);
-    messageInput.value = "";
+function enviarMensagem() {
+    const entradaMensagem = document.getElementById("messageInput");
+    const mensagem = entradaMensagem.value;
+    soquete.send(mensagem);
+    entradaMensagem.value = "";
 }
+
+function executarTesteDeLatencia() {
+    if (soquete && soquete.readyState === WebSocket.OPEN) {
+        tempoInicioTesteLatencia = Date.now();
+        soquete.send("TesteDeLatencia");
+    }
+}
+
+function tratarRespostaTesteLatencia() {
+    if (tempoInicioTesteLatencia) {
+        const latencia = Date.now() - tempoInicioTesteLatencia;
+        const resultadoLatencia = document.getElementById("latencyResult");
+        resultadoLatencia.innerHTML = `Latência: ${latencia} ms`;
+        tempoInicioTesteLatencia = null;
+    }
+}
+
+window.onload = function() {
+    const botaoTesteLatencia = document.getElementById("runLatencyTest");
+    botaoTesteLatencia.addEventListener("click", executarTesteDeLatencia);
+};
